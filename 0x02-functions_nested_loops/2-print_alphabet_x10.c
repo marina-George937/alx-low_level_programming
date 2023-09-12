a035add8cf8e7d39bdff54d677cfc85542760175
marina-george937 <marinageorge937@gmail.com>
2023-09-12 12:45:32 -0700

    main.h

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,6 +10,7 @@
  *
  *
  */
+
 void print_alphabet_x10(void)
 {
 	char a = 'a';
@@ -22,6 +23,7 @@ void print_alphabet_x10(void)
 			_putchar(a);
 			a++;
 		}
+
 		_putchar('\n');
 		i++;
 		a = 'a';
