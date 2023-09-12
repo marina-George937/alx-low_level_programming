f11f6f5a48bc435e4c7d4fe4dbb2ab38275a230c
marina-george937 <marinageorge937@gmail.com>
2023-09-12 12:19:40 -0700

    main.h

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -10,13 +10,13 @@
 
  void print_alphabet(void)
  {
-	 char a = 'a';
-	 while (a <= 'z')
-	 {
-		 _putchar(a);
-		 a++;
-	 }
-	 _putchar('\n');
+	char a = 'a';
+	while (a <= 'z')
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar('\n');
 
 
  }
