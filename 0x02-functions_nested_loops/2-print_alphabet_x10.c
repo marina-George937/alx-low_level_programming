d6b36f05e4c8ae6afb5d7574fc0b5febdb99acd6
marina-george937 <marinageorge937@gmail.com>
2023-09-12 12:34:08 -0700

    main.h

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *
+ * print_alphabet_x10 - prints alphabet 10 times
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char a = 'a';
+	int i = 1;
+
+	while (i <= 10)
+	{
+		while (a <= 'z')
+		{
+			_putchar(a);
+			a++;
+		}
+		
+		_putchar('\n');
+		i++;
+	}
+
+
+
+
+}	
