9da55f36438b0d9e5cd9b53ae7896eccb7400feb
marina-george937 <marinageorge937@gmail.com>
2023-09-12 16:23:33 -0700

    main.h

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -6,6 +6,7 @@
  * @n: the number to get the absolute value to it 
  *
  * Return: 0 or positive number
+ *
  */
 
 int _abs(int n)
@@ -14,6 +15,4 @@ int _abs(int n)
 		return (n);
 	else
 		return (n*-1);
-
-
 }
