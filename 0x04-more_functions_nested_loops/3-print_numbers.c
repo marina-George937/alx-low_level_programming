24c3f56d84d489b1407b1d90ee9028800f949d95
marina-george937 <marinageorge937@gmail.com>
2023-09-14 14:59:50 -0700

    main.h

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,15 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
  * print_numbers - print 0 to 9
  */
 
-
-
 void print_numbers(void)
 {
-	for (int i = 0; i < 10; i++)
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
 		_putchar(i);
+
 	_putchar('\n');
 }
