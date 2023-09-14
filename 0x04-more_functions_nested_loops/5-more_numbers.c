44f08148e5a34a55452a2491a3e1e0a4e2b19cc0
marina-george937 <marinageorge937@gmail.com>
2023-09-14 16:14:22 -0700

    main.h

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * more_numbers - print 0 to 14 ten times
+ */
+
+
+
+void more_numbers(void)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		for (char j = '0'; j < '15'; j++)
+			_putchar(j);
+
+		_putchar('\n');
+	}
+
+
+}
