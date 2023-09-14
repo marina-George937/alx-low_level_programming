9595785c8e0f2d080443196f78e96a3d1ae3b530
marina-george937 <marinageorge937@gmail.com>
2023-09-14 11:05:20 -0700

    main.h

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,20 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+
+
+
+
+
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: character to be checked
+ * Return: 1 if true, 0 if false
+ */
+
+int _isupper(int c);
+
+
+
+#endif
