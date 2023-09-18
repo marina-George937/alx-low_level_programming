18034eaf8b3b9198ac893f9d073b37cd09243dcd
marina-george937 <marinageorge937@gmail.com>
2023-09-18 09:12:38 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,18 +4,17 @@
  * _strlen - return the length of string
  *
  * @s: string to check
+ * Return: length os string
  */
 
 
 
 
 int _strlen(char *s)
-{	
+{
 	int i = 0;
 
-	while (s[i] != '\0' )
+	while (s[i] != '\0')
 		i++;
-
 	return (i);
-
 }
