521dabed098f273d4029d9ad790cee1844f88c04
marina-george937 <marinageorge937@gmail.com>
2023-09-18 12:51:32 -0700

    7-puts_half

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * print_array - prints n elements of an array of integers
+ * @a: array
+ * @n: number of elements
+ */
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		printf("%d, ",a[i]);
+		i++;
+	}
+	printf("\n");
+}
