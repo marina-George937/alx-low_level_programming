2633758150e24da8c2c860c5a5c29e5ab249b8ea
marina-george937 <marinageorge937@gmail.com>
2023-09-20 12:12:58 -0700

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -17,4 +17,16 @@
 char *_strcat(char *dest, char *src);
 
 
+/**
+ * _strncat - concats two arrays
+ *
+ * @dest: destination of concat
+ * @src: source array to concat
+ * @n: amount of times to append
+ *
+ * Return: char value
+ */
+char *_strncat(char *dest, char *src, int n);
+
+
 #endif
