83561508787c7c33c899b002430d598ae652fdd3
marina-george937 <marinageorge937@gmail.com>
2023-09-21 14:06:35 -0700

    str

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -19,7 +19,12 @@ char *_strncpy(char *dest, char *src, int n)
 		dest[i] = src[i];
 		i++;
 	}
+	for (; i < n; i++)
+        {
+                dest[i] = '\0';
+        }
 	
 	return (dest);
 
 }
+
