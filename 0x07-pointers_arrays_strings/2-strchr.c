739f2842c8e84eaf53aed4616a18eb3fa37c8f4d
marina-george937 <marinageorge937@gmail.com>
2023-09-25 10:21:35 -0700

    memcpy

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,10 +1,12 @@
 #include "main.h"
+
 /**
  * _strchr - locates character in string
  * @s: string to locate char
  * @c: character to find
  * Return: character value
  */
+
 char *_strchr(char *s, char c)
 {
 	int i = 0;
