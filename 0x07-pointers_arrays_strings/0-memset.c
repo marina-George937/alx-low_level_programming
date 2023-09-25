3351a24ebed2adab703d832582c39ebeab770816
marina-george937 <marinageorge937@gmail.com>
2023-09-25 09:52:37 -0700

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _memset: set memory
+ *
+ * @s: pointer to vhar
+ * @b: char
+ * @n: int number
+ *
+ * Return: pointer to char
+ */
+
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+
+
+	return s;
+}
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,20 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+
+
+/**
+ * _memset: set memory
+ *
+ * @s: pointer to vhar
+ * @b: char
+ * @n: int number
+ *
+ * Return: pointer to char
+ */
+
+
+char *_memset(char *s, char b, unsigned int n);
+
+
+#endif
