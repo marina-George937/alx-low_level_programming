8dbf39e69f0ea9d6cfe604ba3cd7188038d6d03e
marina-george937 <marinageorge937@gmail.com>
2023-09-28 13:36:18 -0700

    recursion

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,22 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+ * _puts_recursion - uses recursion
+ * @s: string to recurse through
+ */
+
+void _puts_recursion(char *s);
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+
+
+
+#endif
