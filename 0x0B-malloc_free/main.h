8d7a69bfa39bba0a58f9a26ec18f14dc957e8809
marina-george937 <marinageorge937@gmail.com>
2023-10-04 12:56:33 -0700

    malloc

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,14 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+
+/**
+ * create_array - creates array
+ * @size: size of array to create
+ * @c: char to initialize with?
+ * Return: char value
+ */
+char *create_array(unsigned int size, char c);
+
+
+#endif
