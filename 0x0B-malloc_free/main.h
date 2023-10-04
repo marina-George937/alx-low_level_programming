93fac1e599ad3f479ea0c57fdb016845d6acc1a3
marina-george937 <marinageorge937@gmail.com>
2023-10-04 13:42:04 -0700

    malloc

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -38,5 +38,16 @@ char *_strdup(char *str);
  */
 char *str_concat(char *s1, char *s2);
 
+/**
+ * alloc_grid - Returns a pointer to a 2-dimensional array of
+ *               integers with each element initalized to 0.
+ * @width: The width of the 2-dimensional array.
+ * @height: The height of the 2-dimensional array.
+ *
+ * Return: If width <= 0, height <= 0, or the function fails - NULL.
+ *         Otherwise - a pointer to the 2-dimensional array of integers.
+ */
+int **alloc_grid(int width, int height);
+
 
 #endif
