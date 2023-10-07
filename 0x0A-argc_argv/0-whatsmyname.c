8a58107a15deb8a94694be6b5bffab8a56d3274f
marina-george937 <marinageorge937@gmail.com>
2023-10-07 14:07:29 -0700

    argc argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,15 +1,17 @@
 #include "main.h"
 #include <stdio.h>
-#define UNUSED(x) (void)(x)
+
 /**
- * main - main function
- * @argc: argumentc
- * @argv: vector of arguments
- *Return: always 0
- */
+  * main - Prints its name
+  * @argc: argument count
+  * @argv: argument vector
+  *
+  * Return: Always zero
+  */
 int main(int argc, char *argv[])
 {
-	UNUSED(argc);
+	(void) argc;
 	printf("%s\n", argv[0]);
+
 	return (0);
 }
