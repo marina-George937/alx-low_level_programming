848ea98b79a8c9eeae3301fa12497249fae6022c
marina-george937 <marinageorge937@gmail.com>
2023-10-10 13:00:32 -0700

    struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
-
-struct dog{
-char *name;
-float age;
-char *owner;
+/**
+ * struct dog - structure of dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
 };
 #endif
