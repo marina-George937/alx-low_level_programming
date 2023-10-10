77eb5ecd746727e72ae02332113dc62ff07657f6
marina-george937 <marinageorge937@gmail.com>
2023-10-10 13:34:40 -0700

    struct

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * new_dog - creates new dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Return: a dog object
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *n;
+	n = malloc(sizeof(dog_t));
+	if (n != NULL)
+	{
+		n->name = name;
+		n->age = age;
+		n->owner = owner;
+	}
+	return (n);
+}
