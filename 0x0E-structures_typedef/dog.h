4d5b5ae1456a9ec6cccc2f119cd5c436f89bfd2e
marina-george937 <marinageorge937@gmail.com>
2023-10-10 13:07:38 -0700

    struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,4 +12,14 @@ struct dog
 	float age;
 	char *owner;
 };
+
+/**
+ * init_dog - initializes a dog
+ * @d: address of dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
 #endif
