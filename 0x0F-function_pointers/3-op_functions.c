5662f1061f3e0659987ee3f91251e2c38f5f0ae5
marina-george937 <marinageorge937@gmail.com>
2023-10-13 14:08:23 -0700

    pointrr fun

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+ * op_add - adds 5 functions
+ * @a: int a
+ * @b: int b
+ * Return: sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - subtracts
+ * @a: int a
+ * @b: int b
+ * Return: difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplies
+ * @a: int a
+
